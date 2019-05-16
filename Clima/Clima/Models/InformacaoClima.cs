1afddf146234258737ed9b96bcc5ef067a665c6d
Marcelo José da Silva <marcelobill@msn.com>
2019-05-15 22:27:03 -0300

    Correção no retorno da api e correção no cadastro de cidades

--- Clima/Clima/Models/InformacaoClima.cs
+++ Clima/Clima/Models/InformacaoClima.cs
@@ -4,57 +4,18 @@ using System.Text;
 
 namespace Clima.Models
 {
-
 	public class InformacaoClima
 	{
-		//public Coord coord { get; set; }
 		public Weather[] weather { get; set; }
-		//public string _base { get; set; }
 		public Main main { get; set; }
-		//public int visibility { get; set; }
-		//public Wind wind { get; set; }
-		//public Clouds clouds { get; set; }
-		//public int dt { get; set; }
-		//public Sys sys { get; set; }
-		//public int id { get; set; }
 		public string name { get; set; }
-		//public int cod { get; set; }
-	}
-
-	public class Coord
-	{
-		public float lon { get; set; }
-		public float lat { get; set; }
 	}
 
 	public class Main
 	{
 		public float temp { get; set; }
-		public int pressure { get; set; }
-		public int humidity { get; set; }
-		public int temp_min { get; set; }
-		public int temp_max { get; set; }
-	}
-
-	public class Wind
-	{
-		public float speed { get; set; }
-		public int deg { get; set; }
-	}
-
-	public class Clouds
-	{
-		public int all { get; set; }
-	}
-
-	public class Sys
-	{
-		public int type { get; set; }
-		public int id { get; set; }
-		public float message { get; set; }
-		public string country { get; set; }
-		public int sunrise { get; set; }
-		public int sunset { get; set; }
+		public float temp_min { get; set; }
+		public float temp_max { get; set; }
 	}
 
 	public class Weather
@@ -64,5 +25,4 @@ namespace Clima.Models
 		public string description { get; set; }
 		public string icon { get; set; }
 	}
-
 }
